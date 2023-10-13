53790c62c0df9deb02a9cb64c0f0a70f302da5f0
Sebastian Gilits <sep.gil@gmail.com>
2023-10-13 13:58:22 +0200

    Scaffold trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import { serverClient } from "./_trpc/serverClient";
 
-export default function Home() {
+export default async function Home() {
+  const data = await serverClient.getData();
+  const dataSet = await serverClient.setData("test-data");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UserButton afterSignOutUrl="/" />
+      {/* <UserButton afterSignOutUrl="/" /> */}
+      <div>{data}</div>
+      <div>{dataSet}</div>
     </main>
   );
 }
