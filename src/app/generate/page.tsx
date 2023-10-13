cd1432309c79c08c9a15a1fb2340b915bcb8d027
Sebastian Gilits <sep.gil@gmail.com>
2023-10-13 16:06:10 +0200

    Improve desktop max width

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -2,7 +2,7 @@ import { Editor } from "@/components/Editor";
 
 export default async function Generate() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4  max-w-2xl m-auto">
       <Editor />
     </main>
   );
