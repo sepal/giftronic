dcda47967b0a5094e1f0c013fb107f64fbbe3b8f
Sebastian Gilits <sep.gil@gmail.com>
2023-10-24 20:07:19 +0200

    Hide sign in/up button when logged in

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,37 +1,53 @@
 import Image from "next/image";
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
-import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
+import { UserButton, auth, currentUser } from "@clerk/nextjs";
+import { Suspense } from "react";
+import { Skeleton } from "../ui/skeleton";
 
-const Header = () => (
-  <header className="p-2 bg-gray-100">
-    <div className="flex justify-around md:justify-between items-center">
-      <Link href={"/"}>
-        <Image
-          className="h-6 md:h-8 w-auto"
-          src="/logo.png"
-          alt="Giftronic"
-          width={662}
-          height={125}
-        />
-      </Link>
+const Header = () => {
+  const { userId } = auth();
 
-      <div className="flex gap-2">
-        <Link
-          className={buttonVariants({ variant: "secondary" })}
-          href={"/sign-in"}
-        >
-          Login
+  console.log(userId);
+
+  return (
+    <header className="p-2 bg-gray-100">
+      <div className="flex justify-between items-center h-8">
+        <Link href={"/"}>
+          <Image
+            className="h-6 md:h-8 w-auto"
+            src="/logo.png"
+            alt="Giftronic"
+            width={662}
+            height={125}
+          />
         </Link>
-        <Link
-          href={"/sign-up"}
-          className={buttonVariants({ variant: "default" })}
-        >
-          Sign up
-        </Link>
+
+        <div className="flex gap-2">
+          {userId ? (
+            <>
+              <UserButton />
+            </>
+          ) : (
+            <>
+              <Link
+                className={buttonVariants({ variant: "secondary" })}
+                href={"/sign-in"}
+              >
+                Login
+              </Link>
+              <Link
+                href={"/sign-up"}
+                className={buttonVariants({ variant: "default" })}
+              >
+                Sign up
+              </Link>
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export { Header };
