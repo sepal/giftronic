635565ce0a7836fc14275344f5266748dd105b75
Sebastian Gilits <sep.gil@gmail.com>
2023-10-29 21:55:49 +0100

    Add MemeTextEditor story

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -5,7 +5,7 @@ import { BellAlertIcon } from "@heroicons/react/24/solid";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: "Example/Button",
+  title: "UI/Button",
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
@@ -21,7 +21,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
+export const Default: Story = {
   render: (args) => {
     let child: any = "Button";
     if (args.size == "icon") {
