f78df825659d87ac6006dd45fb0f72c25a8464e5
Sebastian Gilits <sep.gil@gmail.com>
2023-10-29 21:49:12 +0100

    Add storybook

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -30,7 +30,7 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  },
+  }
 );
 
 export interface ButtonProps
@@ -41,6 +41,7 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
+    console.log(variant);
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
@@ -49,7 +50,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       />
     );
-  },
+  }
 );
 Button.displayName = "Button";
 
