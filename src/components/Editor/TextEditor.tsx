97fdcd5722f7aa06ddb34c16f70fb9e0cbf80e93
Sebastian <sep.gil@gmail.com>
2023-10-30 18:42:44 +0100

    Fix not able to add spaces on fifo (#11)

--- src/components/Editor/TextEditor.tsx
+++ src/components/Editor/TextEditor.tsx
@@ -2,45 +2,45 @@
 
 import { VideoText } from "@/lib/hooks/useFFMPEG";
 import { MemeTextInput } from "../ui/memeTextInput";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 interface Props {
   onTextChange: (texts: VideoText[]) => void;
   defaultText?: VideoText[];
 }
 
-const TextEditor = ({ onTextChange, defaultText = [] }: Props) => {
-  const topText = useRef<string>("");
-  const bottomText = useRef<string>("");
-  const handleTextChange = () => {
-    const texts = [
-      { text: topText.current, x: 0, y: 46 },
-      { text: bottomText.current, x: 0, y: 312 },
-    ];
-    onTextChange(texts);
-  };
+const TextEditor = ({ onTextChange, defaultText = undefined }: Props) => {
+  const [texts, setTexts] = useState<VideoText[]>(
+    defaultText || [
+      { text: "", x: 0, y: 46 },
+      { text: "", x: 0, y: 312 },
+    ]
+  );
 
   return (
-    <div className="relative w-full z-10 h-full grid grid-rows-3 grid-flow-col gap-4 items-center ">
-      <div className="">
-        <MemeTextInput
-          defaultText={defaultText[0]?.text || "Top text"}
-          onChange={(text) => {
-            topText.current = text;
-            handleTextChange();
-          }}
-        />
-      </div>
+    <div className="relative w-full z-10 h-full grid grid-rows-3 grid-flow-col gap-4 items-center text-white text-3xl text-center">
+      <MemeTextInput
+        defaultText={defaultText ? defaultText[0]?.text : ""}
+        placeholder="Enter a top text..."
+        onChange={(text) => {
+          const newTexts = [...texts];
+          newTexts[0].text = text;
+
+          setTexts(newTexts);
+          onTextChange(newTexts);
+        }}
+      />
       <div></div>
-      <div>
-        <MemeTextInput
-          defaultText={defaultText[1]?.text || "Top text"}
-          onChange={(text) => {
-            bottomText.current = text;
-            handleTextChange();
-          }}
-        />
-      </div>
+      <MemeTextInput
+        defaultText={defaultText ? defaultText[1]?.text : ""}
+        placeholder="Enter a bottom text..."
+        onChange={(text) => {
+          const newTexts = [...texts];
+          newTexts[1].text = text;
+          setTexts(newTexts);
+          onTextChange(newTexts);
+        }}
+      />
     </div>
   );
 };
